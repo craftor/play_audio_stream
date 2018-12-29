6f02a55c1e34ad4cc250e712489c059f2877498c
Craftor <craftor@126.com>
2018-12-30 00:30:27 +0800

    update

--- play_audio.py
+++ play_audio.py
@@ -1,32 +1,5 @@
 
-import pygame
-import sys
-from urllib import request
 
-#http://public.craftor.org/no.wav
-#"http://public.craftor.org/bgm.mp3"
+http://public.craftor.org/no.wav
+http://public.craftor.org/bgm.mp3
 
-url = "http://public.craftor.org/no.wav"
-#url = "http://public.craftor.org/bgm.mp3"
-
-filename = url.split('/')[-1]
-
-# with request.urlopen(url) as web:
-#     # 为保险起见使用二进制写文件模式，防止编码错误
-#     with open('no.wav', 'wb') as outfile:
-#         outfile.write(web.read())
-
-#pygame.init()
-pygame.mixer.init()
-screen=pygame.display.set_mode([640,480])
-#pygame.time.delay(1000)
-pygame.mixer.music.load('bgm.mp3')
-pygame.mixer.music.play(0, 0.0)
-
-# while 1:
-#     for event in pygame.event.get():
-#         if event.type==pygame.QUIT:
-#             sys.exit()
-
-# sound = pygame.mixer.Sound('bgm.mp3')
-# sound.play()
--- play_server.py
+++ play_server.py
@@ -12,17 +12,102 @@ from flask import url_for
 from flask import Flask
 app = Flask(__name__)
 
+# 初始化pygame
+pygame.mixer.init()
+#screen=pygame.display.set_mode([640,480])
+
 # Index
 @app.route('/')
 def index():
     return "Server OK"
 
+# 检查音乐是否正在播放
+@app.route('/get_busy')
+def get_busy():
+    try:
+        result = pygame.mixer.music.get_busy()
+        return str(result)
+    except Exception:
+        return "Failed"
+
+# 暂停
+@app.route('/pause')
+def pause():
+    try:
+        pygame.mixer.music.pause()
+        return "OK"
+    except Exception:
+        return "Failed"
+
+# 取消暂停
+@app.route('/unpause')
+def unpause():
+    try:
+        pygame.mixer.music.unpause()
+        return "OK"
+    except Exception:
+        return "Failed"
+
+# 设置音量 0.0-1.0
+@app.route('/set_volume')
+def set_volume():
+
+    try:
+        value = float(request.args.get('value'))
+        if (value <= 1.0) and (value >= 0.0):
+            pass
+        else:
+            return "Failed"
+    except Exception:
+        return "Failed"
+
+    try:
+        pygame.mixer.music.set_volume(value)
+    except Exception:
+        return "Failed"
+
+    return "OK"
+
+# 播放序列，每次只能添加一个
+@app.route('/queue')
+def queue():
+    try:
+        # 获取URL
+        file_url = request.args.get('file_name')
+        # 截取文件名
+        filename = file_url.split('/')[-1]
+    except Exception:
+        return "Failed"
+
+    # 下载音乐文件
+    try:
+        with urllib.request.urlopen(file_url) as web:
+            # 为保险起见使用二进制写文件模式，防止编码错误
+            try:
+                with open(filename, 'wb+') as outfile:
+                    outfile.write(web.read())
+                    outfile.close()
+            except Exception:
+                pass
+    except Exception:
+        return "Failed"
+
+    try:
+        pygame.mixer.music.queue(filename)
+    except Exception:
+        return "Failed"
+
+    return "OK"
+
 # 删除音乐缓存文件
 @app.route('/remove', methods=['GET', 'POST'])
 def clear():
-    # 截取文件名
-    filename = request.args.get('file_name')
-    print(filename)
+    # 获取文件名
+    try:
+        filename = request.args.get('file_name')
+    except Exception:
+        return "Failed"
+    # 删除文件
     try:
         os.remove(filename)
         return "OK"
@@ -32,31 +117,64 @@ def clear():
 # 播放音乐
 @app.route('/play', methods=['GET', 'POST'])
 def play():
-    # 获取URL
-    file_url = request.args.get('play_file_name')
-    # 截取文件名
-    filename = file_url.split('/')[-1]
+
+    try:
+        # 获取URL
+        file_url = request.args.get('file_name')
+        # 截取文件名
+        filename = file_url.split('/')[-1]
+    except Exception:
+        return "Failed"
+
+    # 获取重复次数
+    try:
+        loop = int(request.args.get('loop'))
+        if (loop < -1):
+            return "Failed"
+    except Exception:
+        loop = 0
+
+    # 获取播放音量
+    try:
+        volume = float(request.args.get('volume'))
+        if (volume <= 1.0) and (volume >= 0.0):
+            pass
+        else:
+            return "Failed"
+    except Exception:
+        volume = 1.0
     
     # 下载音乐文件
-    with urllib.request.urlopen(file_url) as web:
-        # 为保险起见使用二进制写文件模式，防止编码错误
-        try:
-            with open(filename, 'wb+') as outfile:
-                outfile.write(web.read())
-                outfile.close()
-        except Exception:
-            pass
+    try:
+        with urllib.request.urlopen(file_url) as web:
+            # 为保险起见使用二进制写文件模式，防止编码错误
+            try:
+                with open(filename, 'wb+') as outfile:
+                    outfile.write(web.read())
+                    outfile.close()
+            except Exception:
+                pass
+    except Exception:
+        return "Failed"
 
-    # 初始化pygame
-    pygame.mixer.init()
-    screen=pygame.display.set_mode([640,480])
-
-    #print(f.getvalue())
     # 读入音频文件
-    pygame.mixer.music.load(filename)
-    # 播放
-    pygame.mixer.music.play(0, 0.0)
-    print(filename)
+    try:
+        pygame.mixer.music.load(filename)
+    except Exception:
+        return "Failed"
+
+    try:
+        # 设置音量
+        pygame.mixer.music.set_volume(volume)
+    except Exception:
+        return "Failed"
+
+    try:
+        # 播放
+        pygame.mixer.music.play(loop, 0.0)
+    except Exception:
+        return "Failed"
+
     return "OK"
 
 if __name__ == '__main__':
